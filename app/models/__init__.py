9899358f3c40afbb9961b9a7abd3cfca4f1fde5b
Ryan Harden <ryanharden20@gmail.com>
2023-02-21 10:47:25 -0800

    models

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,10 @@
 from .db import db
 from .user import User
+from .address import Address
+from .order_details import OrderDetail
+from .order import Order
+from .product_images import ProductImage
+from .product import Product
+from .review_images import ReviewImage
+from .review import Review
 from .db import environment, SCHEMA
