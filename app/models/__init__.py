cfaf2b456988a0d4c24d21334cbdca84367da203
Ryan Harden <ryanharden20@gmail.com>
2023-02-22 08:35:00 -0800

    update

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -5,7 +5,6 @@ from .order_details import OrderDetail
 from .order import Order
 from .product_images import ProductImage
 from .product import Product
-from .review_images import ReviewImage
 from .review import Review
 from .cart_item import CartItem
 from .db import environment, SCHEMA
