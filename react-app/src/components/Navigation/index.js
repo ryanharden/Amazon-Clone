5668b8effed8d484b6066cfdd63fc2943c97e64b
Ryan Harden <ryanharden20@gmail.com>
2023-02-22 13:41:29 -0800

    some of nav

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -1,23 +1,47 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
+import flag from "../../assets/american-flag.png";
 
-function Navigation({ isLoaded }){
+function Navigation({ isLoaded }) {
 	const sessionUser = useSelector(state => state.session.user);
 
 	return (
-		<ul>
-			<li>
-				<NavLink exact to="/">Home</NavLink>
-			</li>
-			{isLoaded && (
-				<li>
-					<ProfileButton user={sessionUser} />
-				</li>
-			)}
-		</ul>
+		<div className="nav-bar-container">
+			<div className="nav-bar">
+				<div className="header-left">
+					<div className="logo">
+						<NavLink className="nav-bar-nav-link" exact to="/">
+							Rainforest Retail
+						</NavLink>
+					</div>
+					{/* <div className='deliver-to-address'>
+					</div> */}
+				</div>
+				{/* <div className='search-bar-container'>
+
+				</div> */}
+				<div className="header-right">
+					{isLoaded && (
+						<div className="lang-profile-orders-cart">
+							<div className='flag-lang'>
+								<img src={flag} alt="flag"/>
+								<div className='en'>EN</div>
+							</div>
+							<div className="profile-button-container">
+								<ProfileButton user={sessionUser} />
+							</div>
+							{/* <div className='orders-button-container'>
+							</div> */}
+							{/* <div className='cart-button-container'>
+							</div> */}
+						</div>
+					)}
+				</div>
+			</div>
+		</div>
 	);
 }
 
