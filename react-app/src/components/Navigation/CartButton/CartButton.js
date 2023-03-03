8102fbd05c34dd7aada73469bfc94b1762a7e9aa
Ryan Harden <ryanharden20@gmail.com>
2023-03-02 22:48:55 -0800

    cartbutton quantity fixed

--- react-app/src/components/Navigation/CartButton/CartButton.js
+++ react-app/src/components/Navigation/CartButton/CartButton.js
@@ -1,25 +1,42 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
+import setUser from "../../../store/session";
 import "./CartButton.css";
+import { getCartItemsThunk } from "../../../store/cartitem";
 
 const CartButton = () => {
     const cartItems = useSelector(state => state.CartItems);
     const cartItemsArr = Object.values(cartItems);
+    const user = useSelector(state => state.session.user);
+    const [quantity, setQuantity] = useState(0);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        if (user) {
+            // If there is a user, dispatch the getCartItemsThunk to get the cart items for the current user
+            dispatch(getCartItemsThunk());
+        }
+    }, [dispatch, user]);
+
+    useEffect(() => {
+        // Update the cart quantity when the cartItems state changes
+        setQuantity(findQuantity(Object.values(cartItems)));
+    }, [cartItems]);
 
     const findQuantity = (arr) => {
-        let quantity = 0;
+        let totalQuantity = 0;
         arr.forEach(item => {
-            quantity += item.quantity
+            totalQuantity += item.quantity
         })
-        return quantity
+        return totalQuantity
     };
 
-    const quantity = findQuantity(cartItemsArr);
+    // const quantity = findQuantity(cartItemsArr);
 
     return (
         <Link to="/cart" className="cart-button-container">
-            <div className="cart-quantity">{quantity}</div>
+            <div className="cart-quantity">{user ? quantity : 0}</div>
             <div className="cart-cart-image">
                 <div className="cart-image" />
                 <div className="cart-title">Cart</div>
