21b3d20962fe966c0e144f3cffd0d4d1ca1c42e6
Ryan Harden <ryanharden20@gmail.com>
2023-03-25 13:12:42 -0700

    review form

--- app/forms/review_form.py
+++ app/forms/review_form.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,DecimalField,IntegerField
-from wtforms.validators import DataRequired, Email, ValidationError,Length,NumberRange
+from wtforms.validators import DataRequired, Email, ValidationError, SubmitField, Length,NumberRange
 
 class ReviewForm(FlaskForm):
-    review_image_url = StringField("Upload Image URL")
     rating = IntegerField("Rating", validators=[DataRequired(), NumberRange(min=1)])
     headline = StringField("Headline")
     body = StringField("Body", validators=[DataRequired(), Length(min=1,max=5000, message='Reviews be between 1 and 5000 characters')])
+    submit = SubmitField()
